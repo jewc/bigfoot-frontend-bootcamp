4bdef3cd07937654a3b111c1391fa956dd587e78
jewc <jamescew@gmail.com>
2023-12-05 07:16:46 -0500

    File reorganization

---
+++ frontend/src/index.js
@@ -0,0 +1,25 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
+import App from "./components/App";
+import Sighting from "./components/Sighting";
+import SightingPreviewList from "./components/SightingPreviewList";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <BrowserRouter>
+    <Routes>
+      {/* Route that provides base app UI */}
+      <Route path="/" element={<App />}>
+        {/* Route that renders all sightings */}
+        <Route index element={<SightingPreviewList />} />
+        {/* Route that renders individuals sightings */}
+        <Route path="sightings/:sightingIndex" element={<Sighting />} />
+        {/* Route that matches all other paths */}
+        <Route path="*" element={"Nothing here!"} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
+);
